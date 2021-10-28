da8271c861f66e684e8ba04f08de5a925366e827
Andre Mauri <79666075+andremauri@users.noreply.github.com>
2021-10-28 15:27:05 -0400

    Add files via upload

---
+++ Aula 5 - Funções/script.js
@@ -0,0 +1,12 @@
+/*function funcao() {
+    console.log('Essa mensagem veio de uma função!')
+}
+
+funcao();*/
+
+//função com parametros 
+function mensagem (primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Tudo certo', 'jovem' );
