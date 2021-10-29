bc9d2431fa0ed49ab245c01a1723e101de7a8bbb
Andre Mauri <79666075+andremauri@users.noreply.github.com>
2021-10-29 16:48:48 -0400

    Add files via upload

---
+++ Aula 2 - Variaveis/script.js
@@ -0,0 +1,93 @@
+//tipos primitivos 
+
+//boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var funcao = function() {}
+console.log(typeof(funcao));*/
+
+//como delcarar
+/*var variavel = 'andre';
+variavel = 'mauri';
+console.log(variavel);
+
+let variavel2 = 'andre';
+console.log(variavel2);*/
+
+/*const constante = 'andre';
+console.log (constante);*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log (escopoLocalInterno);
+}
+
+escopoLocal();*/
+
+//atribuição
+/*var atribuição = 'andre'
+ 
+//comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//comparação identica 
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);
+
+//adição 
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira); 
+
+//potencição
+var potenciacao = 2 ** 10
+console.log(potenciacao);*/
+
+/*//maior que 
+var maiorQue = 5 > 2;
+console.log(maiorQue);*/
+
+/*//menor 
+var menorQue = 5 < 2;
+console.log(menorQue);*/
+
+/*//maior ou igual a 
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);*/
+
+/*//menor ou igual
+var menorOuIgual = 5 =< 2;
+console.log(menorOuIgual);*/
+
+/*var ou = false || false;
+console.log(ou);*/
+
+var nao = !true;
+console.log(nao);
+
+
