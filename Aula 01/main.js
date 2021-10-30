9e9303b78969a1ef8c24037cd2825c6281f3d5a8
Andre Mauri <79666075+andremauri@users.noreply.github.com>
2021-10-30 06:55:20 -0400

    Add files via upload

---
+++ Aula 01/main.js
@@ -0,0 +1,14 @@
+var nome = "Andre Willian";
+var idade = 25;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+//replace -> altera a variavel 
+//toUpperCase -> deixa tudo com letra maiscula 
+//toLowerCase -> deixa tudo com letra minuscula 
+
---
+++ Aula 02 - Array e Dicionario/main.js
@@ -0,0 +1,27 @@
+var lista = ["maça","pêra","laranja"];
+console.log(lista[1]);
+alert(lista[1]);
+
+//adicionar item a lista
+lista.push("uva");
+
+//tirar item da lista
+lista.pop("uva");
+
+//ver o tamanho da lista
+console.log(lista.length);
+
+//altera a ordem dos itens na lista
+console.log(lista.reverse());
+
+//transforma a array em string
+console.log(lista.toString());
+
+//altera as virgulas dentro da array por - ou outros como |
+console.log(lista.join());
+
+//dicionario
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
